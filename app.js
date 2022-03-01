e2dc8f8fe29371d64479b84f4a6964af34118a51
Hacker_Master01 <56979864+HackerMaster01@users.noreply.github.com>
2022-03-01 09:28:35 +0100

    j

--- app.js
+++
@@ -1,8 +0,0 @@
-function send() {
-    var name = document.getElementById("name")
-    var text = document.getElementById("text")
-
-    console.log(name.value + "  " + text.innerHTML)
-
-    text.innerHTML = "Velkommen, vi ønsker deg alt som er godt, " + name.value + "!";
-}
