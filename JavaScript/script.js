ae074b5cc55aaed47ef514b4e5a8833126ee2837
Hacker_Master01 <56979864+HackerMaster01@users.noreply.github.com>
2022-03-24 18:09:34 +0100

    f

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,17 +1,14 @@
 // send 
 function send() {
-    localStorage.setItem("navn", document.getElementById("name"))
-    localStorage.setItem("text", document.getElementById("text"))
+    localStorage.setItem("name", document.getElementById('name').value)
 
-    console.log(localStorage.getItem("name") + "  " + localStorage.getItem("text"))
+    console.log(localStorage.getItem("name"))
 
-    text.innerHTML = "Velkommen, vi ønsker deg alt som er godt, " + localStorage.getItem("name") + "!";
-}   
+    document.getElementById("text").innerHTML = "Velkommen, vi ønsker deg alt som er godt, " + localStorage.getItem("name") + "!";
+} 
+
 // set random number
-        document.getElementById("random").innerHTML = Math.floor(Math.random() * 10)
-    setInterval(function() {
-        document.getElementById("random").innerHTML = Math.floor(Math.random() * 10)
-    }, 60 * 1000);
+
 
 function random() {
     var num = document.getElementById("num")
@@ -19,6 +16,8 @@ function random() {
 
     if (num.value === random.innerHTML) {
         document.body.style.backgroundColor = "green";
+    } else {
+
     }
 
 
