8d6bf7da91481c8e36526c06cdb38b4bb252ea85
Hacker_Master01 <56979864+HackerMaster01@users.noreply.github.com>
2022-03-24 20:25:28 +0100

    dddd

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,5 @@
+
+
 // send 
 function send() {
     localStorage.setItem("name", document.getElementById('name').value)
@@ -7,23 +9,57 @@ function send() {
     document.getElementById("text").innerHTML = "Velkommen, vi ønsker deg alt som er godt, " + localStorage.getItem("name") + "!";
 } 
 
-// set random number
-
-        document.getElementById("random").innerHTML = Math.floor(Math.random() * 10)
-    setInterval(function() {
-        document.getElementById("random").innerHTML = Math.floor(Math.random() * 10)
-        console.log(document.getElementById("random"))
-    }, 60 * 1000);
 
 function random() {
     var num = document.getElementById("num")
     var random = document.getElementById("random")
 
     if (num.value === random.innerHTML) {
-        document.body.style.backgroundColor = "green";
+        document.body.style.backgroundColor = randomcolor();
+        document.getElementById("random").innerHTML = Math.floor(Math.random() * 10)
     } else {
-        document.body.style.backgroundColor = "wight";
+        document.body.style.backgroundColor = "white";
     }
 
 
 }
+
+function randomcolor() {
+    var n = Math.floor(Math.random() * 10)
+
+    if (n === 0) {
+        return "red";
+    } else if(n === 1) {
+        return "blue";
+    } else if(n === 2) {
+        return "yellow";
+    } else if(n === 3) {
+        return "green";
+    } else if(n === 4) {
+        return "purple";
+    } else if(n === 5) {
+        return "pink";
+    } else if(n === 6) {
+        return "orange";
+    } else if(n === 7) {
+        return "darkgray";
+    } else if(n === 8) {
+        return "lightcoral";
+    } else if(n === 9) {
+        return "gray";
+    }
+ 
+}
+
+// next page
+
+function namegame() {
+    window.location.href = "index.html";
+}
+
+function randomgame() {
+    console.log("hfhfhf")
+    window.location.href = "random.html";
+}
+
+
