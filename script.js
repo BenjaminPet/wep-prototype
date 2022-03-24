18ef0444b101a308899a4370837d829d0265f330
Hacker_Master01 <56979864+HackerMaster01@users.noreply.github.com>
2022-03-24 08:27:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+function send() {
+        var name = document.getElementById("name")
+        var text = document.getElementById("text")
 
+        console.log(name.value + "  " + text.innerHTML)
+
+        text.innerHTML = "Velkommen, vi ønsker deg alt som er godt, " + name.value + "!";
+    }   
+
+        document.getElementById("random").innerHTML = Math.floor(Math.random() * 10)
+    setInterval(function() {
+        document.getElementById("random").innerHTML = Math.floor(Math.random() * 10)
+    }, 60 * 1000);
+
+    function random() {
+        var num = document.getElementById("num")
+        var random = document.getElementById("random")
+
+        if (num.value === random.innerHTML) {
+            document.body.style.backgroundColor = "green";
+        }
+
+
+    }
